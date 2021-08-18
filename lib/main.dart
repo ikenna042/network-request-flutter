8719c2d74f8df7ecceef0e1f1b5909b9a4de8b8b
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-08-18 14:59:50 +0100

    Made get request and returned as text on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:network_request/data_service.dart';
+// import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -10,10 +12,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _dataService = DataService();
+  String _response;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Builder(builder: (_) {
+              if (_response != null) {
+                return Text(_response);
+              } else {
+                return ElevatedButton(
+                    onPressed: _makeRequest, child: Text('Make Request'));
+              }
+            }),
+          ),
+        ),
+      ),
     );
+  }
+
+  void _makeRequest() async {
+    final response = await _dataService.makeRequest();
+    setState(() => _response = response);
   }
 }
